185c8b26a71048fd606920a4565a69354fb2b213
Kruchyna Elena <kruchynaelena@gmail.com>
2021-11-11 20:11:27 +0200

    files project

--- js/promise/promise-1.js
+++ js/promise/promise-1.js
@@ -1,4 +1,4 @@
-const delay = ms => {
+/*const delay = ms => {
     // Твой код
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -6,9 +6,23 @@ const delay = ms => {
         }, -ms)
     });
 }
-const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = time => console.log(`Resolved after ${time}ms`);*/
+
+const delay = (ms) => {
+    const myPromis = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(ms);
+      }, ms);
+    });
+    return myPromis;
+  };
+  
+  const logger = (time) => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
+
+  
